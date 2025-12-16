3221637f5975ab8ceaee2b7cf51dc5d9b7d086d0
hugo-HDSF <hugo.dasilva.filipe@gmail.com>
2025-12-16 13:22:13 +0100

    feat: add characters

---
+++ apps/my-app/app/characters/page.tsx
@@ -0,0 +1,49 @@
+type character = {
+    id: number;
+    name: string;
+    status: string;
+    species: string;
+    type: string;
+    image: string;
+}
+
+type charactersResponse = {
+    info: {
+        count: number;
+        pages: number;
+        next: string | null;
+        prev: string | null;
+    }
+    results: character[];
+
+}
+
+export default async function charactersPage() {
+    const characters = async () => {
+        const res = await fetch("https://rickandmortyapi.com/api/character");
+        return await res.json();
+    }
+
+    const charactersResponse: charactersResponse = await characters();
+    console.log("data", charactersResponse);
+
+    return (
+        // use images and make a baisic grid layout
+        <div
+            //className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 p-4"
+            //make only 3 caracters per row
+            className="flex gap-4 p-4 justify-center flex-wrap"
+        >
+            {charactersResponse.results.map((character) => (
+                <div key={character.id} className="bg-white rounded-lg shadow-md p-4 w-1/4">
+                    <img src={character.image} alt={character.name}
+                         className="w-full h-48 object-cover rounded-md mb-4"/>
+                    <h2 className="text-xl font-bold mb-2">{character.name}</h2>
+                    <p className="text-gray-700">Status: {character.status}</p>
+                    <p className="text-gray-700">Species: {character.species}</p>
+                    <p className="text-gray-700">Type: {character.type || "N/A"}</p>
+                </div>
+            ))}
+        </div>
+    );
+}
